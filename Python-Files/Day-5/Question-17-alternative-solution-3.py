f6b0e32cf03c7a28e7b71764e16e48ae404f0f73
Nihat Halici <nihat.halici@gmail.com>
2021-07-13 09:54:45 +0200

    Add files via upload

---
+++ Python-Files/Day-5/Question-17-alternative-solution-3.py
@@ -0,0 +1,41 @@
+"""
+Question 17
+Question:
+Write a program that computes the net amount of a bank account based a transaction log
+from console input. The transaction log format is shown as following:
+
+D 100
+W 200
+
+D means deposit while W means withdrawal.
+
+Suppose the following input is supplied to the program:
+
+D 300
+D 300
+W 200
+D 100
+
+Then, the output should be:
+
+500
+
+Hints:
+In case of input data being supplied to the question, it should be assumed to be a console input."""
+
+'''Solution by: popomaticbubble 
+'''
+transactions = []
+
+while True:
+    text = input("> ")
+    if text:
+        text = text.strip('D ')
+        text = text.replace('W ', '-')
+        transactions.append(text)
+    else:
+        break
+
+transactions = (int(i) for i in transactions)
+balance = sum(transactions)
+print(f"Balance is {balance}")
---
+++ Python-Files/Day-5/Question-17-alternative-solution-4.py
@@ -0,0 +1,54 @@
+"""
+Question 17
+Question:
+Write a program that computes the net amount of a bank account based a transaction log
+from console input. The transaction log format is shown as following:
+
+D 100
+W 200
+
+D means deposit while W means withdrawal.
+
+Suppose the following input is supplied to the program:
+
+D 300
+D 300
+W 200
+D 100
+
+Then, the output should be:
+
+500
+
+Hints:
+In case of input data being supplied to the question, it should be assumed to be a console input."""
+
+'''Solution by: ChichiLovesDonkeys
+'''
+
+money = 0
+while 1:
+    trans = input().split(' ')
+    if trans[0] == 'D':
+        money = money + int(trans[1])
+    elif trans[0] == 'W':
+        money = money - int(trans[1])
+    elif input() == '':
+        break
+    print(f'Your current balance is: {money}')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
