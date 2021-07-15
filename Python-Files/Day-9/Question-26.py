b7fc878c2b6b1b10e6175e78013c30607bd22b4b
Nihat Halici <nihat.halici@gmail.com>
2021-07-15 00:46:23 -0700

    Create Question-26.py

---
+++ Python-Files/Day-9/Question-26.py
@@ -0,0 +1,12 @@
+"""
+Question 26
+Question:
+Define a function which can compute the sum of two numbers.
+
+Hints:
+Define a function with two numbers as arguments.
+You can compute the sum in the function and return the value.
+
+"""
+sum = lambda n1, n2: n1 + n2
+print(sum(1,2))
