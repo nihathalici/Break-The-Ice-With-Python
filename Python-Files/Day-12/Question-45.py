435436c5a339bd4cbf7dbb90b49130e1ae296f5d
Nihat Halici <nihat.halici@gmail.com>
2021-07-16 10:34:27 +0200

    Add files via upload

---
+++ Python-Files/Day-12/Question-45.py
@@ -0,0 +1,25 @@
+"""
+Question 45
+Question:
+Define a class named American which has a static method called printNationality.
+
+Hints:
+Use @staticmethod decorator to define class static method.
+There are also two more methods.To know more, go to:
+https://realpython.com/instance-class-and-static-methods-demystified/.
+"""
+
+class American():
+    @staticmethod
+    def printNationality():
+        print("I am American")
+
+american = American()
+
+# this will not run if @staticmethod does not decorates the function.
+# Because the class has no instance.
+american.printNationality()
+
+# this will run even though the @staticmethod
+# does not decorate printNationality()
+American.printNationality()
