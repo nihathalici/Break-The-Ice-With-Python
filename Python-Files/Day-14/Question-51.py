a63b3acffb931adab8aab8d603f85a9d29c18c30
Nihat Halici <nihat.halici@gmail.com>
2021-07-16 01:37:14 -0700

    Create Question-51.py

---
+++ Python-Files/Day-14/Question-51.py
@@ -0,0 +1,18 @@
+"""
+Question 51
+Question
+Write a function to compute 5/0 and use try/except to catch the exceptions.
+
+Hints
+Use try/except to catch exceptions.
+"""
+
+def divide():
+    return 5/0
+
+try:
+    divide()
+except ZeroDivisionError as ze:
+    print("Why on earth you are dividing a number by ZERO!!")
+except:
+    print("Any other exception")
