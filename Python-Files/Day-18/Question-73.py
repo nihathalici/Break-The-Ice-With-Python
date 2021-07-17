0f4dfc8a6b278893cc4a0797e111ed7f258d2317
Nihat Halici <nihat.halici@gmail.com>
2021-07-17 11:40:15 +0200

    Add files via upload

---
+++ Python-Files/Day-18/Question-73.py
@@ -0,0 +1,13 @@
+"""
+Question 73
+Question
+Please write a program to randomly generate a list
+with 5 even numbers between 100 and 200 inclusive.
+
+Hints
+Use random.sample() to generate a list of random values.
+"""
+
+import random
+
+print(random.sample([i for i in range(100, 201) if i % 2 == 0], 5))
