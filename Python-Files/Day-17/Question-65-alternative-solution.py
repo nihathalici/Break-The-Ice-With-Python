a5f6bd785cc442d026d1c2b7119dd7aaf742f7a1
Nihat Halici <nihat.halici@gmail.com>
2021-07-17 11:38:24 +0200

    Add files via upload

---
+++ Python-Files/Day-17/Question-65-alternative-solution.py
@@ -0,0 +1,19 @@
+"""
+Question 65
+Question
+Please write assert statements to verify that every number
+in the list [2,4,6,8] is even.
+
+Hints
+Use "assert expression" to make assertion.
+"""
+
+data = [2,4,5,6]
+
+for i in data:
+    assert i % 2 == 0, "{} is not an even number.".format(i)
+
+
+
+
+
---
+++ Python-Files/Day-17/Question-66.py
@@ -0,0 +1,24 @@
+"""
+Question 66
+Question
+Please write a program which accepts basic mathematic expression
+from console and print the evaluation result.
+
+Example: If the following n is given as input to the program:
+
+35 + 3
+
+Then, the output of the program should be:
+
+38
+
+Hints
+Use eval() to evaluate an expression.
+"""
+
+
+expression = input()
+print(eval(expression))
+
+
+
